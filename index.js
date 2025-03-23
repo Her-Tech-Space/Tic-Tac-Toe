a2a7d52ef97dd5b80c2c7206b005ea52fb063636
Her Tech Space <her.t3ch.space@gmail.com>
2025-03-23 18:44:37 +0000

    Added the game board to the game

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//Defining the game board since this'll be played in the terminal 
+//Think of the spaces as the boxes in the game
+let gameBoard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
+//creating a variable that is the current player 
+let currentPlayer = '🫀';
+
+//Making sure the game keeps going til there is a loser/winner
+let gameActive = true;
+
+
+function printBoard() {
+    console.log(`
+      ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}
+      ---------
+      ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}
+      ---------
+      ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}
+    `);
+  }
